443a5e1393dd40de4001f196bfc361edf4a373d1
oliversenti <41137630+oliversenti@users.noreply.github.com>
2022-12-27 11:49:16 +0100

    removed key

--- earthelevation.py
+++ earthelevation.py
@@ -1,13 +1,14 @@
 import requests
 import json
+import Constants 
 
 
-key = API_KEY
+key = Constants.API_KEY
 
 def calcElevation(lat,lon):
     lat = str(lat)
     lon = str(lon)
-    url = "https://maps.googleapis.com/maps/api/elevation/json?locations="+lat+","+lon+apikey
+    url = "https://maps.googleapis.com/maps/api/elevation/json?locations="+lat+","+lon+"&key="+key
     print(url, "this is the gogole api url")
     json_response = requests.get(url).json()
     elevation = json_response['results'][0]['elevation']
